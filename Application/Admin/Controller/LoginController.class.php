14c642d58620d8020fddba361c08e94a14c325c5
RussellKevin <liucong3764306@gmail.com>
2016-08-17 21:39:11 +0800

    project init

---
+++ Application/Admin/Controller/LoginController.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+
+/**
+ * use Common\Model 这块可以不需要使用，框架默认会加载里面的内容
+ */
+class LoginController extends Controller {
+
+    public function index(){
+
+    	return $this->display();
+    }
+
+}
---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+
+    //URL地址不区分大小写
+    'URL_CASE_INSENSITIVE' =>true,
+    'URL_MODEL'=>0,
+);
